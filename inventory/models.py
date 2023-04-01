f72182601abc450e54c71f43b1d19d81cf355753
Daniel Holter <daniel.holter@outlook.com>
2023-04-01 17:21:26 +0200

    added status model and updated item model

--- inventory/models.py
+++ inventory/models.py
@@ -3,9 +3,24 @@ from django.db import models
 # Create your models here.
 
 
+class Status(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Item(models.Model):
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=10000)
+    desc = models.CharField(max_length=10000, blank=True)
+    spec = models.CharField(max_length=200, blank=True)
+    cpu = models.CharField(max_length=200, blank=True)
+    mem = models.CharField(max_length=200, blank=True)
+    disk0 = models.CharField(max_length=200, blank=True)
+    disk1 = models.CharField(max_length=200, blank=True)
+    disk2 = models.CharField(max_length=200, blank=True)
+    disk3 = models.CharField(max_length=200, blank=True)
+    status = models.CharField(max_length=200, blank=True)
     created_at = models.DateTimeField('Created Time', auto_now_add=True)
     updated_at = models.DateTimeField('Updated Time', auto_now=True)
     # make = models.ForeignKey(Make, on_delete=models.CASCADE)
@@ -18,7 +33,7 @@ class Item(models.Model):
 
 class Part(models.Model):
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=10000)
+    desc = models.CharField(max_length=10000, blank=True)
     created_at = models.DateTimeField('Created Time', auto_now_add=True)
     updated_at = models.DateTimeField('Updated Time', auto_now=True)
     # make = models.ForeignKey(Make, on_delete=models.CASCADE)
@@ -31,7 +46,7 @@ class Part(models.Model):
 
 class Task(models.Model):
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=10000)
+    desc = models.CharField(max_length=10000, blank=True)
     finished = models.BooleanField(null=True)
     created_at = models.DateTimeField('Created Time', auto_now_add=True)
     updated_at = models.DateTimeField('Updated Time', auto_now=True)
@@ -42,7 +57,7 @@ class Task(models.Model):
 
 class Make(models.Model):
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=10000)
+    desc = models.CharField(max_length=10000, blank=True)
     created_at = models.DateTimeField('Created Time', auto_now_add=True)
     updated_at = models.DateTimeField('Updated Time', auto_now=True)
 
@@ -52,7 +67,7 @@ class Make(models.Model):
 
 class Model(models.Model):
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=10000)
+    desc = models.CharField(max_length=10000, blank=True)
     created_at = models.DateTimeField('Created Time', auto_now_add=True)
     updated_at = models.DateTimeField('Updated Time', auto_now=True)
     make = models.ForeignKey(Make, on_delete=models.CASCADE)
@@ -63,12 +78,12 @@ class Model(models.Model):
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=10000)
+    desc = models.CharField(max_length=10000, blank=True)
     created_at = models.DateTimeField('Created Time', auto_now_add=True)
     updated_at = models.DateTimeField('Updated Time', auto_now=True)
     # make = models.ForeignKey(Make, on_delete=models.CASCADE)
     # model = models.ForeignKey(Model, on_delete=models.CASCADE)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    #task = models.ForeignKey(Task, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
