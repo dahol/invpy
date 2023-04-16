805ea216a50c19d3343ec35be2796369f31b2f71
Daniel Holter <daniel.holter@outlook.com>
2023-04-16 21:55:29 +0200

    added basic tests

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class ItemTestCase(TestCase):
+    def setUp(self):
+        Item.objects.create(name="Itemtest1", desc="Itemtest desc1")
+        Item.objects.create(name="Itemtest2", desc="Itemtest desc2")
+        
+class TaskTestCase(TestCase):
+    def setUp(self):
+        Task.objects.create(name="Tasktest1", desc="Tasktest desc1")
+        Task.objects.create(name="Tasktest2", desc="Tasktest desc2")
+
