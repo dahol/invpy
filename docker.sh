18fab25534d2b502cfe570e7c1690b45c95934b4
Daniel Holter <daniel.holter@husbanken.no>
2023-06-05 14:47:17 +0200

    added sudo

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
-docker build . -t dahol/invpy
-docker stop invpy # if already running
-docker rm invpy # if already running
-docker run -p 80:8000 --name invpy -d dahol/invpy
+sudo docker build . -t dahol/invpy
+sudo docker stop invpy # if already running
+sudo docker rm invpy # if already running
+sudo docker run -p 80:8000 --name invpy -d dahol/invpy
