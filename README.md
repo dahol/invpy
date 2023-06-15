ce12063bdbe2bfc3f74819179aa1be816282ad9e
Daniel Holter <daniel.holter@outlook.com>
2023-06-15 11:08:46 +0200

    buildscript and updated readme

--- README.md
+++ README.md
@@ -45,3 +45,27 @@ pip freeze > .\requirements.txt
 run docker.sh
 
 ```
+
+### Build scipt
+
+## How to run
+
+- Kubernetes
+
+  - Build.sh
+
+    - Arguments:
+      - `-b true` to build
+      - `-p true` to push
+      - ex: `./build.sh -b true -p true`
+
+  - Deploy.sh
+
+    - Arguments:
+      - `-u username`
+      - `-p password`
+      - `-h host`
+      - `-d database`
+      - `-p port`
+
+- Docker compose (tba)
---
+++ build.sh
@@ -0,0 +1,30 @@
+
+
+
+while getopts b:p: flag
+do
+    case "${flag}" in
+        b) build=${OPTARG};;
+        p) push=${OPTARG};;
+    esac
+done
+
+# Build
+
+if [ "$build" = "true" ] ; then
+    echo "Building.."
+    docker build . -t dahol/invpy
+    echo "Done building!"
+    elif [ "$build" = "false" ] ; then
+    echo "Skipping build.."
+fi
+
+# Push
+
+if [ "$push" = "true" ] ; then
+    echo "Pushing.."
+    docker push dahol/invpy
+    echo "Done pushing!"
+    elif [ "$push" = "false" ] ; then
+    echo "Skipping push.."
+fi
